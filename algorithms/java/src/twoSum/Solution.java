253d80af92aef3e8f1c423f7495f09289aedc398
Stefanie Sinner <s.sinner@campus.lmu.de>
2025-06-28 20:56:38 +0200

    JAVA #1 TASK: Add description of the task

--- algorithms/java/src/twoSum/Solution.java
+++ algorithms/java/src/twoSum/Solution.java
@@ -1,5 +1,32 @@
 package twoSum;
 
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+ * target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * <p>
+ * <b>Example 1:</b>
+ * <br>
+ * Input: nums = [2, 7, 11, 15], target = 9
+ * <br>
+ * Output: [0, 1]
+ * <br>
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * <p>
+ * <b>Example 2:</b>
+ * <br>
+ * Input: nums = [3, 2, 4], target = 6
+ * <br>
+ * Output: [1, 2]
+ *
+ * <p>
+ * <b>Example 3:</b>
+ * <br>
+ * Input: nums = [3, 3], target = 6
+ * <br>
+ * Output: [0, 1]
+ */
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         return nums;
