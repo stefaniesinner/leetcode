08a11dde53034bfb7adf06713a60405f4c2a3e05
Stefanie Sinner <stefanie_sinner@gmx.de>
2025-06-30 09:23:17 +0200

    JAVA #1 TASK: Remove redundant assignment in method twoSum

--- algorithms/java/src/twoSum/Solution.java
+++ algorithms/java/src/twoSum/Solution.java
@@ -53,11 +53,7 @@ public class Solution {
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        int start = 0;
-
         for (int i = 0; i < nums.length; i++) {
-            start = nums[i];
-
             for (int j = 0; j < nums.length; j++) {
                 if (i == j) {
                     break;
