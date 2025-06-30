5a4d4928dbce9f7203443e6a44c44ed72bbfb183
Stefanie Sinner <stefanie_sinner@gmx.de>
2025-06-30 15:31:11 +0200

    JAVA #2 TASK: Reverse the first input using Collections and save progress

--- algorithms/java/src/addTwoNumbers/Solution.java
+++ algorithms/java/src/addTwoNumbers/Solution.java
@@ -1,8 +1,35 @@
 package addTwoNumbers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Solution {
 
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = new Solution().addTwoNumbers(l1, l2);
+        System.out.println(result);
+    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode();
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        while (l1 != null) {
+            System.out.println(l1.val);
+            list1.add(l1.val);
+            l1 = l1.next;
+        }
+
+        Collections.reverse(list1);
+        System.out.println(list1);
+
+        return l1;
     }
 }
